293eb03d9af50fcbc40f063ad705c030c217dd00
BeiDouShin <h.yamamoto.nyaki@gmail.com>
2025-06-05 00:33:19 +0900

    初回コミット

---
+++ src/test/java/jp/co/volante/volante/VolanteApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.volante.volante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
